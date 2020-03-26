a0db37780ae8939887004e31ee79bc2bc8040e54
Colin MacLeod <colinlmacleod1@gmail.com>
2020-03-25 21:07:52 -0400

    feat: Create basic frontend

---
+++ web/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './chef.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Cooooooooooook
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          And Boooooooook
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
