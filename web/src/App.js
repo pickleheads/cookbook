0e533aa80b63f81d00f426cf8a02eb5775745d85
Zack Harley <zackharley@gmail.com>
2020-03-28 17:38:31 -0400

    feat: Add Prettier - Format existing files - Add Prettier auto-run task to JetBrains IDEs on Save - Add basic gitignore

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './chef.png';
-import './App.css';
+import React from "react";
+import logo from "./chef.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Cooooooooooook
-        </p>
+        <p>Cooooooooooook</p>
         <a
           className="App-link"
           href="https://reactjs.org"
