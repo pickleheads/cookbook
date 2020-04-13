313957feb6430429b13712a04bc6aabfda2c3705
Lucas Manuel <lucas.manuel@ca.ey.com>
2020-04-13 14:19:03 -0400

    feat: add styled-components
    fix: missed removing a className
    
    App.js, from the link component
    fix: missing semicolon
    
    on App.js import

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,53 @@
-import React from "react";
-import logo from "./chef.png";
-import "./App.css";
+import React from 'react';
+import logo from './chef.png';
+import styled, { keyframes } from 'styled-components';
 
+const AppContainter = styled.div`
+  text-align: center;
+`
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+const AppLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${AppLogoSpin} infinite 20s linear;
+  }
+`
+const AppLink = styled.a`
+  color: #61dafb;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppContainter>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo"/>
         <p>Cooooooooooook</p>
-        <a
-          className="App-link"
+        <AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          And Boooooooook
-        </a>
-      </header>
-    </div>
+        And Boooooooook
+        </AppLink>
+      </AppHeader>
+    </AppContainter>
   );
 }
 
--- web/src/index.js
+++ web/src/index.js
@@ -1,14 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
