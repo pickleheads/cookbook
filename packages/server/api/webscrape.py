97766005972a2cc2762fa4c9d977ce8f45af8424
Lucas Manuel <lucas.manuel@ca.ey.com>
2020-04-18 13:58:23 -0400

    feat: add whitelisted url for bonappetit, webscraping for ingeedients

--- packages/server/api/webscrape.py
+++ packages/server/api/webscrape.py
@@ -1,20 +1,41 @@
 from http.server import BaseHTTPRequestHandler
+import urllib.request as request
+import re
 from urllib.parse import parse_qs
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
 class handler(BaseHTTPRequestHandler):
-
+  whitelisted_url_regexes = [
+    re.compile("^https:\/\/www.bonappetit.com")
+  ]
   def do_GET(self):
     queryString = self.path.split('webscrape?')
     if(len(queryString) > 1 and 'url=' in queryString[1]):
-        query = parse_qs(queryString[1])
-        self.send_response(200)
+      query = parse_qs(queryString[1])
+      url = query['url'][0]
+
+      if(not any(regex.match(url) for regex in self.whitelisted_url_regexes)):
+        self.send_response(500)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(str(query['url'][0]).encode())
+        self.wfile.write('Unsupported recipe site'.encode())
+        return
+
+      fp = request.urlopen(url)
+      mybytes = fp.read()
+      html_doc = mybytes.decode("utf8")
+      fp.close()
+
+      soup = BeautifulSoup(html_doc, 'html.parser')
+      ingredients = soup.find("div", "ingredients")
+      self.send_response(200)
+      self.send_header('Content-type', 'text/plain')
+      self.end_headers()
+      self.wfile.write(str(ingredients.prettify()).encode())
     else:
         self.send_response(500)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write('Missing url parameter'.encode())
-
     return
