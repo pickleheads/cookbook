2426925f6e54c7312e2bfdae03acc5619ce0f815
Lucas Manuel <lucas.manuel@ca.ey.com>
2020-04-18 17:47:19 -0400

    fix: updated with requested changes from zack

--- packages/server/api/webscrape.py
+++ packages/server/api/webscrape.py
@@ -1,40 +1,32 @@
 from http.server import BaseHTTPRequestHandler
 import urllib.request as request
 import re
-from urllib.parse import parse_qs
+from urllib import parse
 from bs4 import BeautifulSoup
 
 class handler(BaseHTTPRequestHandler):
   whitelisted_url_regexes = [
-    re.compile("^https:\/\/www.bonappetit.com")
+    re.compile("^https:\/\/www\.bonappetit\.com")
   ]
   def do_GET(self):
-    queryString = self.path.split('webscrape?')
-    if(len(queryString) > 1 and 'url=' in queryString[1]):
-      query = parse_qs(queryString[1])
-      url = query['url'][0]
-
-      if(not any(regex.match(url) for regex in self.whitelisted_url_regexes)):
-        self.send_response(500)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('Unsupported recipe site'.encode())
-        return
-
-      fp = request.urlopen(url)
-      mybytes = fp.read()
-      html_doc = mybytes.decode("utf8")
-      fp.close()
-
-      soup = BeautifulSoup(html_doc, 'html.parser')
-      ingredients = soup.find("div", "ingredients")
-      self.send_response(200)
+    url = parse.parse_qs(parse.urlsplit(self.path).query)['url'][0]
+    url_in_whitelist = any(regex.match(url) for regex in self.whitelisted_url_regexes)
+    if(not url_in_whitelist):
+      self.send_response(500)
       self.send_header('Content-type', 'text/plain')
       self.end_headers()
-      self.wfile.write(str(ingredients.prettify()).encode())
-    else:
-        self.send_response(500)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write('Missing url parameter'.encode())
+      self.wfile.write('Unsupported recipe site'.encode())
+      return
+
+    file_pointer = request.urlopen(url)
+    html_bytes = file_pointer.read()
+    html_doc = html_bytes.decode("utf8")
+    file_pointer.close()
+
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    ingredients = soup.find("div", "ingredients")
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write(str(ingredients.prettify()).encode())
     return
